9a74080351470baca29eb022040cb491f87e5aea
Sk Firdous <firdous132487@gmail.com>
2026-03-14 12:34:28 +0530

    bug fixed

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("This is home route");
 })
 
